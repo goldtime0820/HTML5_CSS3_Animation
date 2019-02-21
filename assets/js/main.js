a6f19e782c14e9773fcd63936c599eeb39090793
DESKTOP-5UEQ9PJ\GOLD <goldtime0820@gmail.com>
2019-02-21 14:19:27 -0500

    Update not work good

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,11 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 let flg_end_hide = false,
-  flg_end_show
+  flg_end_show,
+  flg_start_hide,
+  flg_start_show,
   ang_arr = [],
-  end_arr = [],
+  now_finish_show_idx,
   letter_array = [],
   now_words_hide = 0,
   now_words_show = 0,
@@ -23,28 +25,35 @@ let flg_end_hide = false,
         .get()
         .reverse()
     ).each(function(index2) {
+      if (index1) $(this).css({ opacity: 0.0 });
       letter_array[index1][index2] = this;
-      ang_arr[index1][index2] = end_arr[index1][index2] = 0;
+      ang_arr[index1][index2] = 0;
     });
   });
   console.log(letter_array);
 
   $("button").click(function() {
-    hide_letters();
+    start_hide();
   });
 })(jQuery);
 
-const create_animation_hide = (obj, word, letter, ed_op) => {
+const create_animation_hide = (obj, word, letter, n_op) => {
   let step = Math.floor(Math.random() * 360) - 360.0,
     speed = main_speed + Math.random() * 1.2,
     tm = Math.abs(step / speed);
   ang_arr[word][letter] = step + ang_arr[word][letter];
+
+  if (letter == ang_arr[word].length - 1 && n_op == 1.0) {
+    start_show();
+  }
+
+  if (letter == 0 && n_op == 0) flg_end_hide = true;
   if (flg_end_hide) ang_arr[word][letter] = 0;
-  if (flg_end_hide){
+  if (flg_end_hide) {
     $(obj).animate(
       {
         transform: "rotateY(" + ang_arr[word][letter] + "deg)",
-        opacity: ed_op
+        opacity: 0.0
       },
       tm,
       function() {
@@ -52,46 +61,75 @@ const create_animation_hide = (obj, word, letter, ed_op) => {
         // create_animation(obj, word, letter,ed_op);
       }
     );
-  } else{
+  } else {
+    if (flg_start_hide) n_op = Math.max(0.0, n_op - 0.2);
+    console.log("hide", word, letter, n_op);
     $(obj).animate(
       {
-        transform: "rotateY(" + ang_arr[word][letter] + "deg)"
+        transform: "rotateY(" + ang_arr[word][letter] + "deg)",
+        opacity: n_op
       },
       tm,
       function() {
         // Animation complete.
-        create_animation_hide(obj, word, letter, ed_op);
+        create_animation_hide(obj, word, letter, n_op);
       }
     );
-  }  
+  }
 };
-const create_animation_show = (obj, word, letter,n_op) => {
+const create_animation_show = (obj, word, letter, n_op) => {
   let step = Math.floor(Math.random() * 360) - 360.0,
     speed = main_speed + Math.random() * 1.2,
     tm = Math.abs(step / speed);
   ang_arr[word][letter] = step + ang_arr[word][letter];
-  if(flg_end_show && (letter == (letter_array[word].length - 1) || end_arr[word][letter+1])){
-    ang_arr[word][letter] = 360 * Math.floor((ang_arr[word][letter] + 359)/360);
-    n_op = 1.0;
 
-  }else{
-    n_op = Math.min(n_op + 0.07,1.0);
-    $(obj).animate({
-      transform : "rotateY(" + ang_arr[word][letter] + "deg)",
-      opacity : 
-    },tm,function(){
-      create_animation_show(obj,word,letter,n_op);
-    });
+  if (letter == 0 && n_op == 1.0) {
+   // setTimeout(start_hide, 5000);
+  }
+
+  if (letter == 0 && n_op > 0.2) flg_start_hide = true;
+  console.log("show", word, letter, n_op);
+
+  if (letter == (letter_array[word].length - 1) && n_op == 1.0) {
+    flg_end_show = true;
+  }
+
+  if (flg_end_show && letter == now_finish_show_idx) {
+    ang_arr[word][letter] =
+      360 * Math.floor((ang_arr[word][letter] + 359) / 360);
+    n_op = 1.0;
+    $(obj).animate(
+      {
+        transform: "rorateY(" + ang_arr[word][letter + "deg)"],
+        opacity: n_op
+      },
+      tm,
+      function() {
+        now_finish_show_idx--;
+      }
+    );
+  } else {
+    n_op = Math.min(n_op + 0.1, 1.0);
+    $(obj).animate(
+      {
+        transform: "rotateY(" + ang_arr[word][letter] + "deg)",
+        opacity: n_op
+      },
+      tm,
+      function() {
+        create_animation_show(obj, word, letter, n_op);
+      }
+    );
   }
 };
 const hide_letters = () => {
   create_animation_hide(
     letter_array[now_words_hide][now_letter_hide],
-    now_words,
-    now_letter++,
-    0.0
+    now_words_hide,
+    now_letter_hide++,
+    1.0
   );
-  if (now_letter < letter_array[now_words_hide].length) {
+  if (now_letter_hide < letter_array[now_words_hide].length) {
     setTimeout(() => {
       hide_letters();
     }, 300);
@@ -100,12 +138,29 @@ const hide_letters = () => {
 const show_letters = () => {
   create_animation_show(
     letter_array[now_words_show][now_letter_show],
-    now_words,
-    letter_array[now_words_show].length - 1 - now_letter++
+    now_words_show,
+    now_letter_show--,
+    0.0
   );
-  if (now_letter < letter_array[now_words_show].length) {
+  if (now_letter_show >= 0) {
     setTimeout(() => {
-      hide_letters();
+      show_letters();
     }, 500);
   }
+};
+
+const start_show = () => {
+  now_words_show = (now_words_show + 1) % letter_array.length;
+  now_finish_show_idx = letter_array[now_words_show].length - 1;
+  now_letter_show = now_finish_show_idx;
+  flg_end_show = false;
+  flg_start_show = false;
+  show_letters();
+};
+const start_hide = () => {
+  now_words_hide = now_words_show;
+  now_letter_hide = 0;
+  flg_start_hide = false;
+  flg_end_hide = false;
+  hide_letters();
 };
